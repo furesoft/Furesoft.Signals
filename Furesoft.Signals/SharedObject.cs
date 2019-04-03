36d26c10b3314653f04bf1c175a8dd5e61da8c4f
furesoft <filmee24@gmail.com>
2019-04-03 14:14:23 +0200

    added shared object core model

---
+++ Furesoft.Signals/SharedObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Furesoft.Signals
+{
+    public class SharedObject<T>
+    {
+        private List<Action<T>> _callbacks = new List<Action<T>>();
+
+        public void OnChanged(Action<T> callback)
+        {
+            _callbacks.Add(callback);
+        }
+
+        public void SetValue(T value)
+        {
+
+        }
+
+        public static SharedObject<T> operator +(SharedObject<T> obj, Action<T> callback)
+        {
+            obj.OnChanged(callback);
+
+            return obj;
+        }
+    }
+}
