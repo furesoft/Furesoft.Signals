8ba6f11fbc5f27f315391ac236509fb7e0b1e527
furesoft <filmee24@gmail.com>
2019-04-05 20:04:16 +0200

    added optional type

---
+++ Furesoft.Signals/Optional.cs
@@ -0,0 +1,58 @@
+﻿namespace Furesoft.Signals
+{
+    public class Optional<T>
+    {
+        public Optional(T value)
+        {
+            if (value != null)
+            {
+                HasValue = true;
+                Value = value;
+            }
+        }
+
+        public Optional()
+        {
+        }
+
+        public T Value { get; }
+        public bool HasValue { get; }
+
+        public static Optional<TValue> Some<TValue>(TValue value)
+        {
+            return new Optional<TValue>(value);
+        }
+
+        public static Optional<T> None => new Optional<T>();
+
+        public static implicit operator bool(Optional<T> opt)
+        {
+            return opt.HasValue;
+        }
+
+        public static implicit operator T(Optional<T> opt)
+        {
+            if (opt.HasValue)
+            {
+                return opt.Value;
+            }
+
+            return default(T);
+        }
+
+        public static implicit operator Optional<T>(bool value) => Optional<T>.None;
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+
+    public static class ObjectExtentions
+    {
+        public static Optional<T> ToOptional<T>(this T target)
+        {
+            return Optional<T>.Some(target);
+        }
+    }
+}
