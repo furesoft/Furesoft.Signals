99aade0bb96411576a67ab1aeda8124d7a8e41a2
furesoft <filmee24@gmail.com>
2019-04-06 16:37:06 +0200

    added core for ipcstream

---
+++ Furesoft.Signals/Streaming/IpcStream.cs
@@ -0,0 +1,50 @@
+﻿using Furesoft.Signals.Core;
+using System.IO;
+
+namespace Furesoft.Signals.Streaming
+{
+    public class IpcStream : Stream
+    {
+        private MemoryMappedFileCommunicator _com;
+
+        internal IpcStream(MemoryMappedFileCommunicator com)
+        {
+            _com = com;
+        }
+
+        public override bool CanRead => true;
+
+        public override bool CanSeek => false;
+
+        public override bool CanWrite => true;
+
+        public override long Length => throw new System.NotImplementedException();
+
+        public override long Position { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+        public override void Flush()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Furesoft.Signals/Streaming/IpcStreamChunk.cs
@@ -0,0 +1,9 @@
+﻿namespace Furesoft.Signals.Streaming
+{
+    internal class IpcStreamChunk
+    {
+        public int Length { get; set; }
+        public int Position { get; set; }
+        public byte[] Buffer { get; set; }
+    }
+}
