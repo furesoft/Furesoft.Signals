8a2cb49b37f24598cd50de553df37d59690914d9
Furesoft <filmee24@gmail.com>
2020-01-27 16:33:46 +0100

    cleanup

--- Furesoft.Signals/Messages/StreamChunk.cs
+++ Furesoft.Signals/Messages/StreamChunk.cs
@@ -5,7 +5,7 @@ namespace Furesoft.Signals.Messages
 {
     public class StreamChunk
     {
-        public byte[] Data { get; set; }
+        public byte[] Data { get; set; } = new byte[0];
         public int ID { get; set; }
         public int Length { get; set; }
     }
