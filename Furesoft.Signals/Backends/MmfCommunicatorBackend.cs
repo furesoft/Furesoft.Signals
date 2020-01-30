b72e88e8bbdb5b16f829d280301c9492db1aa323
Furesoft <filmee24@gmail.com>
2020-01-30 08:27:15 +0100

    implemented communicator backend

--- Furesoft.Signals/Backends/MmfCommunicatorBackend.cs
+++ Furesoft.Signals/Backends/MmfCommunicatorBackend.cs
@@ -1,15 +1,52 @@
-﻿namespace Furesoft.Signals.Backends
+﻿using Furesoft.Signals.Core;
+using System;
+
+namespace Furesoft.Signals.Backends
 {
     public class MmfCommunicatorBackend : ISignalBackend
     {
-        public void OnNewMessage(byte[] data)
+        public long Capacity { get; private set; }
+
+        public string Name { get; private set; }
+
+        public MmfCommunicatorBackend(string name, long capacity)
         {
-            throw new System.NotImplementedException();
+            Name = name;
+            Capacity = capacity;
+        }
+
+        public event Action<byte[]> OnNewMessage;
+
+        public void Initialize(bool isOwner)
+        {
+            if (isOwner)
+            {
+                communicator = new MemoryMappedFileCommunicator(Name, Capacity);
+                communicator.ReadPosition = 2000;
+                communicator.WritePosition = 0;
+                communicator.StartReader();
+            }
+            else
+            {
+                communicator = new MemoryMappedFileCommunicator(Name, Capacity);
+                communicator.WritePosition = 2000;
+                communicator.ReadPosition = 0;
+                communicator.StartReader();
+            }
+
+            communicator.DataReceived += Communicator_DataReceived;
         }
 
         public void Write(byte[] data)
         {
-            throw new System.NotImplementedException();
+            communicator.Write(data);
+        }
+
+        internal MemoryMappedFileCommunicator communicator;
+
+        private void Communicator_DataReceived(object sender, DataReceivedEventArgs e)
+        {
+            OnNewMessage?.Invoke(e.Data);
         }
     }
 }
